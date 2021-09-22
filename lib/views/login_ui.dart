20a8971819575869dc58e5dbd040a98f6dc7f545
s6135410022 <s6135410022@sau.ac.th>
2021-09-22 15:06:53 +0700

    Code v 2

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginUI extends StatefulWidget {
   const LoginUI({Key? key}) : super(key: key);
@@ -280,6 +281,141 @@ class _LoginUIState extends State<LoginUI> {
                   ],
                 ),
               ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.facebookF,
+                ),
+                label: Text(
+                  'Facbook',
+                  style: TextStyle(
+                    fontFamily: 'kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF1877f2),
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(FontAwesomeIcons.googlePlus),
+                label: Text(
+                  'Google',
+                  style: TextStyle(
+                    fontFamily: 'kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFdb4a39),
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(FontAwesomeIcons.appleAlt),
+                label: Text(
+                  'Apple ID',
+                  style: TextStyle(
+                    fontFamily: 'kanit',
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF333333),
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width - 80,
+                    50.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(FontAwesomeIcons.instagram),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Color(0xFF8a3ab9),
+                      ),
+                      fixedSize: Size(
+                        80.0,
+                        80.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(65.0),
+                      ),
+                      primary: Color(0xFF8a3ab9),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.twitter,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: Size(
+                          80.0,
+                          80.0,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusDirectional.circular(65.0),
+                        ),
+                        primary: Color(0xFF00aced)),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      FontAwesomeIcons.linkedinIn,
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Color(0xFF0077b5),
+                      ),
+                      fixedSize: Size(
+                        80.0,
+                        80.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(65.0),
+                      ),
+                      primary: Color(0xFF0077b5),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
             ],
           ),
         ),
